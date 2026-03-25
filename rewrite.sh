3e2bef44beaa8670ef951ba5f6c5194159aea26d
Bobby Thomas Thomas <bobbythomasthomas23@gmail.com>
2026-03-25 21:38:12 +0530

    Add deployment configuration for Vercel and Railway

--- rewrite.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-CORRECT_NAME="bobbythomas985"
-CORRECT_EMAIL="bobbythomasthomas23@gmail.com"
-
-export GIT_COMMITTER_NAME="$CORRECT_NAME"
-export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-export GIT_AUTHOR_NAME="$CORRECT_NAME"
-export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
